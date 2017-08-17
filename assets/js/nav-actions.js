02024bba62f18bbfa22c764d3a55ef16bee26dd1
Ryan <ryanfiller89@gmail.com>
2017-08-17 12:20:45 -0500

    cleans up unused nav-actions

--- assets/js/nav-actions.js
+++ assets/js/nav-actions.js
@@ -1,39 +1,13 @@
 (function mobileNav($) {
     var site = $('.site-wrap');
     var navicon = $('.js-navicon');
-    var mobileNavSelector = $('.js-mobile-nav-selector');
-    var mobileSubnav = $('.js-mobile-subnav');
-    var subnavActive = $('.js-sub-active');
 
     function naviconTap () {
         $(this).closest(site).toggleClass('open')
         $(this).toggleClass('open')
     }
 
-    function mobileSubnavOpen () {
-        $(this).toggleClass('open')
-        $(this).siblings(mobileSubnav).toggleClass('open');
-    }
-
     navicon.on('click', naviconTap)
-    mobileNavSelector.on('click', mobileSubnavOpen)
-
-    if ($(subnavActive).length > 0) {
-        $(subnavActive).closest(mobileSubnav).addClass('open');
-        $(subnavActive).parent().siblings(mobileNavSelector).addClass('open');
-    }
-
-})(window.jQuery);
-
-
-(function mobileDropdown($) {
-    var button = $('.js-subnav-mobile-button');
-
-    function onButtonClick () {
-        $(this).closest('.js-subnav-mobile').toggleClass('open')
-    }
-
-    button.on('click', onButtonClick)
 
 })(window.jQuery);
 
